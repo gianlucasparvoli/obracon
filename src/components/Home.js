1e981421469707f4df664f13433dace6a3c4e74c
Gianluca Sparvoli <tgsparvoli@gmail.com>
2024-05-25 09:57:45 +0200

    Add Swiper to Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,21 @@
 import React from "react";
-//import BannerBackground from "../Assets/home-banner-background.png";
-import BannerImage from "../Assets/home-banner-image.png";
+import BannerBackground from "../Assets/home-banner-background.png";
+//import BannerImage from "../Assets/home-banner-image.png";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="home-banner-container" style={{"margin-top":"6rem" }}>
-        {/* <div className="home-bannerImage-container">
+      <div className="home-banner-container" >
+        <div className="home-bannerImage-container" style={{ "margin-top": "6rem" }}>
           <img src={BannerBackground} alt="" />
-        </div> */}
+        </div>
         <div className="home-text-section">
           <h1 className="primary-heading">
             Obracon
@@ -21,7 +28,51 @@ const Home = () => {
           </button> */}
         </div>
         <div className="home-image-section">
-          <img src={BannerImage} alt="" />
+          {/* <img src={BannerImage} alt="" /> */}
+          <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={'auto'}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+            }}
+            pagination={true}
+            modules={[EffectCoverflow, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
