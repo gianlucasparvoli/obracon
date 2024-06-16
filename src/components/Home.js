d75be347997f27715a8af41cfa78f08faf698adc
Gianluca Sparvoli <tgsparvoli@gmail.com>
2024-06-16 16:43:04 +0200

    Adding lazy-load library

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { Suspense } from 'react'
 import Home1 from "../data/HomeImages/1.webp";
 import Home2 from "../data/HomeImages/2.webp";
 import Home3 from "../data/HomeImages/3.webp";
-//import { Carousel } from "react-responsive-carousel";
 import Carousel from 'react-bootstrap/Carousel';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Home = () => {
-  var imgPath = [Home1, Home2, Home3]
+  var imgPath = [Home1, Home2, Home3];
+  const MyLoader = () => <div>Cargando...</div>;
   return (
     <div style={{ "margin-top": "1rem" }}>
+      <Suspense>
+        <Carousel>
 
-      <Carousel>
-        {imgPath?.map(image =>
-          <Carousel.Item interval={1500}>
+          {imgPath?.map(image =>
+            <Carousel.Item interval={1500}>
 
-            <img src={image} variant="top" alt="card-img" class="d-block img-fluid" type="image/webp"/>
 
-            <Carousel.Caption>
-              {/*  <h3>First slide label</h3>
+              <LazyLoadImage src={image} loader={<MyLoader />} class="d-block img-fluid" type="image/webp" ></LazyLoadImage>
+
+              <Carousel.Caption>
+                {/*  <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-            </Carousel.Caption>
-          </Carousel.Item>
-        )}
-      </Carousel>
-
+              </Carousel.Caption>
+            </Carousel.Item>
+          )}
+        </Carousel>
+      </Suspense>
     </div>
   );
 };
