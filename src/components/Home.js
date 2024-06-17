10df5e4d9cc0a947c309a171b9a1f9e703c52d4d
Gianluca Sparvoli <tgsparvoli@gmail.com>
2024-06-17 18:06:14 +0200

    Adding Firebase storage to the images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,62 @@
-import React, { Suspense } from 'react'
-import Home1 from "../data/HomeImages/1.webp";
-import Home2 from "../data/HomeImages/2.webp";
-import Home3 from "../data/HomeImages/3.webp";
+import React, { createContext, useContext, useEffect, useState } from "react";
+//import Home1 from "../data/HomeImages/1.webp";
+//import Home2 from "../data/HomeImages/2.webp";
+//import Home3 from "../data/HomeImages/3.webp";
 import Carousel from 'react-bootstrap/Carousel';
 import { Img } from 'react-image';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { storage, auth } from '../firebase'; // Import the storage instance
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+import { signInAnonymously } from "firebase/auth";
 
 const Home = () => {
-  var imgPath = [Home1, Home2, Home3];
+  //var imgPath = [Home1, Home2, Home3];
   const MyLoader = () => <div>Cargando...</div>;
+  const [files, setFiles] = useState([]);
+
+  const authenticate = async () => {
+    try {
+      await signInAnonymously(auth);
+    } catch (error) {
+      console.error("Anonymous authentication failed", error);
+    }
+  };
+
+  useEffect(() => {
+    const listFiles = async () => {
+      await authenticate(); 
+      const storageRef = ref(storage, 'HomeImages');
+      try {
+        const res = await listAll(storageRef);
+        const filePromises = res.items.map((itemRef) => getDownloadURL(itemRef));
+        const fileUrls = await Promise.all(filePromises);
+        setFiles(fileUrls);
+      } catch (error) {
+        console.error("Error listing files", error);
+      }
+    };
+
+    listFiles();
+  }, []);
+  
+  var imgPath = files;
+
   return (
     <div style={{ "margin-top": "1rem" }}>
-      <Suspense>
-        <Carousel>
+      <Carousel>
 
-          {imgPath?.map(image =>
-            <Carousel.Item interval={1500}>
+        {imgPath?.map(image =>
+          <Carousel.Item interval={1500}>
 
 
-              <Img src={image} loader={<MyLoader />} class="d-block img-fluid" type="image/webp"  loading="lazy"  decoding="async" />
+            <Img src={image} loader={<MyLoader />} class="d-block img-fluid" type="image/webp" />
 
-              <Carousel.Caption>
-                {/*  <h3>First slide label</h3>
+            <Carousel.Caption>
+              {/*  <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-              </Carousel.Caption>
-            </Carousel.Item>
-          )}
-        </Carousel>
-      </Suspense>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )}
+      </Carousel>
     </div>
   );
 };
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_firebase_apiKey,
+  authDomain: process.env.REACT_APP_firebase_authDomain,
+  projectId: process.env.REACT_APP_firebase_projectId,
+  storageBucket: process.env.REACT_APP_firebase_storageBucket,
+  messagingSenderId: process.env.REACT_APP_firebase_messagingSenderId,
+  appId: process.env.REACT_APP_firebase_appId
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { auth, storage };
